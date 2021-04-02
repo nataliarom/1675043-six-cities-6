9ef9686100a5894893b223dc0ce30e033dc01ee2
Romanova Natalia <natalia@senserix.com>
2021-04-02 12:53:58 +0300

    error handling

--- src/components/favorite-offers-list/favorite-offers-list.jsx
+++ src/components/favorite-offers-list/favorite-offers-list.jsx
@@ -6,30 +6,48 @@ import FavoriteOfferCard from "../favorite-offer-card/favorite-offer-card";
 import {fetchFavoriteHotels} from "../../store/api-actions";
 
 const FavoriteOffers = ({favoriteOffers}) => {
+
+  const getCities = (offers) => (
+    [...new Set(offers.map((offer)=>(offer.city.name)))]
+  );
+
+  const getOffersByCity = (offers) => {
+    let cities = getCities(offers);
+    return cities.reduce((cityToOffers, city) => {
+      cityToOffers[city] = offers.filter((offer) => (offer.city.name === city));
+      return cityToOffers;
+    }, {});
+  };
+
+  const offersByCity = getOffersByCity(favoriteOffers);
+
   return (
     <ul className="favorites__list">
-      <li className="favorites__locations-items">
-        <div className="favorites__locations locations locations--current">
-          <div className="locations__item">
-            <a className="locations__item-link" href="#">
-              <span>Amsterdam</span>
-            </a>
-          </div>
-        </div>
-        <div className="favorites__places">
-          {favoriteOffers.map((offer) => <FavoriteOfferCard
-            title={offer.title}
-            price={offer.price}
-            previewImage={offer.previewImage}
-            type={offer.type}
-            key={offer.id}
-            id={offer.id}
-            rating={offer.rating}
-          />)}
-        </div>
-      </li>
+      {Object.keys(offersByCity).map(
+          (city) => (
+            <li className="favorites__locations-items" key={city}>
+              <div className="favorites__locations locations locations--current">
+                <div className="locations__item">
+                  <a className="locations__item-link" href="#">
+                    <span>{city}</span>
+                  </a>
+                </div>
+              </div>
+              <div className="favorites__places">
+                {offersByCity[city].map((offer) => <FavoriteOfferCard
+                  title={offer.title}
+                  price={offer.price}
+                  previewImage={offer.previewImage}
+                  type={offer.type}
+                  key={offer.id}
+                  id={offer.id}
+                  rating={offer.rating}
+                />)}
+              </div>
+            </li>
+          )
+      )}
     </ul>
-
   );
 };
 
@@ -38,8 +56,8 @@ FavoriteOffers.propTypes = {
 };
 
 
-const mapStateToProps = ({HOTEL}) => ({
-  favoriteOffers: HOTEL.favoriteOffers,
+const mapStateToProps = (state) => ({
+  favoriteOffers: state.HOTEL.favoriteOffers,
 });
 
 const mapDispatchToProps = (dispatch) => ({
--- src/store/hotel/selectors.js
+++ src/store/hotel/selectors.js
@@ -2,14 +2,13 @@ import {NameSpace} from "../root-reducer";
 import {createSelector} from "reselect";
 
 export const getHotels = (state) => state[NameSpace.HOTEL].hotels;
+
 export const getCity = (state) => state[NameSpace.HOTEL].city;
+
 export const getOffersByCity = (city, hotels) => {
   return hotels.filter((hotel) => {
     return hotel.city.name === city.name;
   });
 };
-export const getOffersFilteredByCity = createSelector(
-    [getHotels],
-    getOffersByCity
-);
 
+export const getOffersFilteredByCity = createSelector(getCity, getHotels, getOffersByCity);
